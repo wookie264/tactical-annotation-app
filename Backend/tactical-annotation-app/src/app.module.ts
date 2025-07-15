a162e909d25b3732b6b72a2e2034bf6b035c5821
Oussama Mahfoudhi <mahfoudhi2604@gmail.com>
2025-07-15 12:08:47 +0100

    added Video and Annotation Modules controllers Services

--- Backend/tactical-annotation-app/src/app.module.ts
+++ Backend/tactical-annotation-app/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AnnotationModule } from './annotation/annotation.module';
+import { VideoModule } from './video/video.module';
 
 @Module({
-  imports: [],
+  imports: [AnnotationModule, VideoModule],
   controllers: [AppController],
   providers: [AppService],
 })
