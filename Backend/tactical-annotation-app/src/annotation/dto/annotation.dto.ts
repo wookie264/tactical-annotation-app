170f88fa7f3c1f1743c60a67a26bb5846db21879
Oussama Mahfoudhi <mahfoudhi2604@gmail.com>
2025-07-16 09:58:53 +0100

    removed isDate from dto

--- Backend/tactical-annotation-app/src/annotation/dto/annotation.dto.ts
+++ Backend/tactical-annotation-app/src/annotation/dto/annotation.dto.ts
@@ -1,4 +1,5 @@
-import {IsDate, IsNotEmpty,IsString} from "class-validator"
+/* eslint-disable prettier/prettier */
+import { IsNotEmpty,IsString} from "class-validator"
 
 export class AnnotationDto {
   @IsString()
