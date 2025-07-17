e1930a1123d35e04f098a636068a68a3b16a685c
Oussama Mahfoudhi <mahfoudhi2604@gmail.com>
2025-07-17 11:02:05 +0100

    added user in schema

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -6,9 +6,10 @@ import { VideoModule } from './video/video.module';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { RapportModule } from './rapport/rapport.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AnnotationModule, PrismaModule, VideoModule, PrismaModule, RapportModule],
+  imports: [AnnotationModule, PrismaModule, VideoModule, PrismaModule, RapportModule, AuthModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
