491c3996942b443342f5b62800be81164807191f
Oussama Mahfoudhi <mahfoudhi2604@gmail.com>
2025-07-18 09:44:56 +0100

    added frontend

---
+++ Frontend/src/app/tactical-annotation-form/tactical-annotation-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tactical-annotation-form',
+  imports: [],
+  templateUrl: './tactical-annotation-form.html',
+  styleUrl: './tactical-annotation-form.css'
+})
+export class TacticalAnnotationForm {
+
+}
